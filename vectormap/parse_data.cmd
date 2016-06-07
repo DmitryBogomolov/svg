8988ca0fdb453055c246a62a95c3d644b9edd228
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2016-06-07 10:49:59 +0300

    vectormap samples

---
+++ vectormap/parse_data.cmd
@@ -0,0 +1,3 @@
+md vectormap-data
+node js\vectormap-utils\dx.vectormaputils.node.js shapefiles --output vectormap-data --process-file-content shapefiles\_process_file_content.js --precision 2
+node js\vectormap-utils\dx.vectormaputils.node.js shapefiles --output vectormap-data --precision 2 --json
---
+++ vectormap/readme.txt
@@ -0,0 +1,3 @@
+Copy "Lib/js" directory to the current directory.
+Create "shapefiles" directory and fill it with .shp/.dbf files.
+Run parse_data.cmd.
